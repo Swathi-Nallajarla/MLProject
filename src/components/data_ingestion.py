482f6ca21302d529a0bbd6b18e457979a8de4866
Swathi Nallajarla <p.swathi.sanjeev@gmail.com>
2024-11-27 11:13:24 +0530

    Data transformation  is done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,9 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
 @dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts',"train.csv")
@@ -21,7 +24,7 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
         try:
-            df=pd.read_csv('notebook\data\gym_members_exercise_tracking.csv')
+            df=pd.read_csv('notebook\data\Cleaned_Students_Performance.csv')
             logging.info("read the sataset as dataframe")
 
             os.makedirs(os.path.dirname(self.ingestion_config.test_data_path),exist_ok=True)
@@ -48,4 +51,8 @@ class DataIngestion:
 
 if __name__=="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+
+    data_transformation=DataTransformation()
+    train_arr,test_ar,_r=data_transformation.initiate_data_transformation(train_data,test_data)
